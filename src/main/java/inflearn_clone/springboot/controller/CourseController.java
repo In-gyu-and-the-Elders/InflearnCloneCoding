471037dd04d57bde2f1e3a0c107e1f86eb55ad51
soojin <ssj1737@gmail.com>
2024-11-26 09:28:54 +0900

    20241126 test 지우기

--- src/main/java/inflearn_clone/springboot/controller/CourseController.java
+++ src/main/java/inflearn_clone/springboot/controller/CourseController.java
@@ -1,4 +1,35 @@
 package inflearn_clone.springboot.controller;
 
+import inflearn_clone.springboot.dto.course.CourseDTO;
+import inflearn_clone.springboot.service.CourseSerivce;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/course")
 public class CourseController {
+    private final CourseSerivce courseSerivce;
+    @GetMapping("/list")
+    public String courseList() {
+//        List<CourseDTO> courseList = courseSerivce.courseList();
+//        model.addAttribute("courseList", courseList);
+        return "course/list";
+    }
+
+    // 상세보기 페이지
+    @GetMapping("/view")
+    public String courseView() {
+//        CourseDTO course = courseSerivce.courseView(idx);
+//        model.addAttribute("course", course);
+        return "course/view";
+    }
 }
--- src/main/java/inflearn_clone/springboot/dto/course/CourseDTO.java
+++ src/main/java/inflearn_clone/springboot/dto/course/CourseDTO.java
@@ -1,4 +1,33 @@
 package inflearn_clone.springboot.dto.course;
 
+import groovy.transform.builder.Builder;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Data
+@AllArgsConstructor
+@NotNull
+@Builder
 public class CourseDTO {
+    private int idx;
+    private String category;
+    @NotNull
+    @Size(min = 5, max = 50)
+    private String title;
+    @NotNull
+    @Size(min = 10, max = 1000)
+    private String description;
+    @NotNull
+    private String teacherId;
+    private String regDate;
+    private String displayDate;
+    private String modifyDate;
+    private String deleteDate;
+    private String status;
+    private int price;
+    private String thumbnail;
 }
